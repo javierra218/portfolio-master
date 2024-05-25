f8f2b78486d1016b545f7812f06cf1ae34a55182
javierra218 <javierra@unicauca.edu.co>
2024-05-24 23:05:54 -0500

    chore: Actualizar fuentes a Fira Code en el diseño de la página

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Utiliza 'class' para habilitar el modo oscuro
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'fira-code': ['"Fira Code"', 'monospace'],
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 }
